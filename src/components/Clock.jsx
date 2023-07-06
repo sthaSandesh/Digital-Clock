572aabe384a7890d65bf765eb7a2e31d22f150bf
sthaSandesh <126145622+sthaSandesh@users.noreply.github.com>
2023-07-06 22:31:27 +0545

    date updated

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -29,7 +29,7 @@ export default function Clock() {
     <div className='relative flex-col flex items-center justify-center text-white font-bold'>
       <div className='text-4xl'>Time</div>
       <div className='text-8xl '>{samaye}</div>
-      <div className='text-4xl'>{miti}</div>
+      <div className='text-4xl pt-4'>Date : {miti}</div>
     </div>
   </div>
 </div>

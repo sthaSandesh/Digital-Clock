b37785246128392c083ba176558b61a14e30c311
sthaSandesh <126145622+sthaSandesh@users.noreply.github.com>
2023-07-21 23:54:47 +0545

    ad stopwatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Clock from './components/Clock'
+import Stopwatch from './components/StopWatch'
 
 export default function App() {
   return (
     <div>
       <Clock />
+      <Stopwatch />
     </div>
   )
 }
--- src/components/StopWatch.jsx
+++ src/components/StopWatch.jsx
@@ -1,40 +1,54 @@
-import React from 'react'
+import React, { useState, useRef } from 'react';
 
-export default function StopWatch() {
-    const [time , setTime] = useState(0);
-    const [timerOn , setTimerOn] = useState(false);
-    
-    useEffect(() => {
-        let interval = null;
-        if(timerOn){
-            interval = setInterval(() => {
-                setTime(prevTime => prevTime + 10)
+function Stopwatch() {
+    const [running, setRunning] = useState(false);
+    const [time, setTime] = useState(0);
+    const intervalRef = useRef();
+
+    const handleStartStop = () => {
+        if (running) {
+            clearInterval(intervalRef.current);
+        } else {
+            const startTime = Date.now() - time;
+            intervalRef.current = setInterval(() => {
+                setTime(Date.now() - startTime);
             }, 10);
-        }else{
-            clearInterval(interval);
         }
-        return () => clearInterval(interval);
-    }, [timerOn])
+        setRunning(!running);
+    };
 
+    const handleReset = () => {
+        clearInterval(intervalRef.current);
+        setRunning(false);
         setTime(0);
-        setTimerOn(false);
-    }
-     
-    time = new Date(time);  
-    let centiseconds = ("0" + (Math.floor(time.getMilliseconds() / 10) % 100)).slice(-2);
-    let seconds = ("0" + (Math.floor(time.getSeconds()) % 60)).slice(-2);
-    let minutes = ("0" + (Math.floor(time.getMinutes()) % 60)).slice(-2);
-    let hours = ("0" + Math.floor(time.getHours())).slice(-2);
+    };
 
-    if(hours === '00'){
-        hours = '';
-    }else{
-        hours = hours + ':';
-    }
+    const formatTime = (time) => {
+        const minutes = Math.floor(time / 60000);
+        const seconds = Math.floor((time % 60000) / 1000);
+        const milliseconds = Math.floor((time % 1000) / 10);
 
-  return (
-    <div>
+        return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${milliseconds.toString().padStart(2, '0')}`;
+    };
 
-    </div>
-  )
+    return (
+        <div className='min-h-screen bg-[url(/src/assets/space.jpg)] bg-cover flex items-center justify-center'>
+            <div className='relative'>
+                <div className='absolute top-[-50%] left-[-50%] w-[200%] h-[200%] backdrop-filter backdrop-blur-md bg-opacity-10 rounded-full'></div>
+                <div className='relative flex-col flex items-center justify-center text-white font-bold'>
+                    <h1 className='text-4xl'>Stopwatch</h1>
+                    <div className="timer text-8xl">{formatTime(time)}</div>
+                    <div className="buttons space-x-10 text-2xl">
+                        <button onClick={handleStartStop}
+                        className='bg-purple-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded-full'
+                        >{running ? 'Stop' : 'Start'}</button>
+                        <button onClick={handleReset}
+                        >Reset</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
+
+export default Stopwatch;
